c87815d6193dd4f957b22fa83e9e31d12b2290e0
Dan O'Reilly <dangoreilly@gmail.com>
2022-03-21 23:30:46 +0000

    initial commit

--- routes/get.js
+++ routes/get.js
@@ -6,11 +6,39 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-  res.render('getBuilding');
+  res.redirect('../');
 
 });
 
 router.get('/all', function(req, res, next) {
+
+  let protoBuilding = {
+    "id": 1,
+    "attributes": {
+        "Name": "Graduate Memorial Building",
+        "bldID": "graduate-memorial-building",
+        "Description": "No description available",
+        "SensoryOverview": "No Sensory Overview",
+        "ZoomedInLabel": "GMB",
+        "ZoomedOutLabel": "GMB",
+        "geometry": null,
+        "sensoryAvailable": false,
+        "aka": "GMB",
+        "Sight": "No description available",
+        "Smell": "No description available",
+        "Sound": "No description available",
+        "Touch": "No description available",
+        "Movement": null,
+        "Tips": "No tips for this area",
+        "infoPageEnabled": true,
+        "createdAt": "2022-03-21T20:24:10.107Z",
+        "updatedAt": "2022-03-21T20:24:15.148Z",
+        "publishedAt": "2022-03-21T20:24:15.144Z",
+        "locale": "en"
+    }
+  };
+
+  
 
   building.find({}, (err, buildings) => {
     if (err){
