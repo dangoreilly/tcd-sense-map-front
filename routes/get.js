020b1b644469c04b2da6dbb7ec576647d52b3a46
Dan O'Reilly <dangoreilly@gmail.com>
2022-03-22 00:25:02 +0000

    Updated get route to connect to strapi

--- routes/get.js
+++ routes/get.js
@@ -1,6 +1,14 @@
 var express = require('express');
-const building = require('../models/building');
+// const building = require('../models/building');
 var router = express.Router();
+const axios = require('axios');
+const dotenv = require('dotenv').config();
+
+let token = process.env.STRAPI_BEARER;
+
+const config = {
+  headers: { Authorization: `Bearer ${token}` }
+};
 
 //var addBuildingRouter = require('./routes/addBuilding');
 
@@ -12,53 +20,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/all', function(req, res, next) {
 
-  let protoBuilding = {
-    "id": 1,
-    "attributes": {
-        "Name": "Graduate Memorial Building",
-        "bldID": "graduate-memorial-building",
-        "Description": "No description available",
-        "SensoryOverview": "No Sensory Overview",
-        "ZoomedInLabel": "GMB",
-        "ZoomedOutLabel": "GMB",
-        "geometry": null,
-        "sensoryAvailable": false,
-        "aka": "GMB",
-        "Sight": "No description available",
-        "Smell": "No description available",
-        "Sound": "No description available",
-        "Touch": "No description available",
-        "Movement": null,
-        "Tips": "No tips for this area",
-        "infoPageEnabled": true,
-        "createdAt": "2022-03-21T20:24:10.107Z",
-        "updatedAt": "2022-03-21T20:24:15.148Z",
-        "publishedAt": "2022-03-21T20:24:15.144Z",
-        "locale": "en"
-    }
-  };
-
-  
-
-  building.find({}, (err, buildings) => {
-    if (err){
-        res.json({
-            confirmation:"fail",
-            error:err,
-            //building:building
-        });
-
-        console.log(err);
-        
-        return;
-    }
-
-    console.log("buildings read successfully from server");
-    //console.log(buildings)
+  axios.get('https://tcd-sense-map-back-zssh2.ondigitalocean.app/api/buildings', config)
+  .then(function (response) {
+    // handle success
+    console.log(response.status);
+    let buildings = response.data;
     res.json(buildings);
+    console.log("buildings read successfully from server");
 
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
     return;
-
   });
 
 });
